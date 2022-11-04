40eee2526d5821a1c8647a845216de36a732c288
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-11-04 03:21:41 -0600

    Re-build express-cart with new axios settings

--- html/express-cart-build/server.js
+++ html/express-cart-build/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use((request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*")
-    response.header("Access-Control-Allow-Methods", "POST, GET, DELETE")
+    response.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE")
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
@@ -26,7 +26,7 @@ class Database {
         const products = require("./server-resources/products")
 
         for (const product of products) {
-            const response = await axios.post(`${baseUrl}/api/products`, product)
+            const response = await axios.post(`/express-cart-api/products`, product)
             if (response.status !== 200) {
                 console.log(`Error adding ${product.name}, code ${response.status}`)
             }
@@ -110,42 +110,42 @@ class CartItem {
     }
 }
 
-app.get('/api/products', (request, response) => {
+app.get('/express-cart-api/products', (request, response) => {
     response.send(database.getProducts())
 })
-app.post('/api/products', (request, response) => {
+app.post('/express-cart-api/products', (request, response) => {
     const newProduct = new Product(request.body.name, request.body.price)
     database.addProduct(newProduct)
     response.send(newProduct)
 })
 
-app.get('/api/products/:id', (request, response) => {
+app.get('/express-cart-api/products/:id', (request, response) => {
     const id = parseInt(request.params.id)
     const product = database.getProductById(id)
     response.send(product)
 })
-app.delete('/api/products/:id', (request, response) => {
+app.delete('/express-cart-api/products/:id', (request, response) => {
     const id = parseInt(request.params.id);
     database.deleteProduct(id)
     response.sendStatus(200);
 })
 
-app.get('/api/cart', (request, response) => {
+app.get('/express-cart-api/cart', (request, response) => {
     response.send(database.getCart())
 })
 
-app.post('/api/cart/:id', (request, response) => {
+app.post('/express-cart-api/cart/:id', (request, response) => {
     const id = parseInt(request.params.id)
     database.addProductToCart(id)
     response.sendStatus(200)
 })
-app.delete('/api/cart/:id', (request, response) => {
+app.delete('/express-cart-api/cart/:id', (request, response) => {
     const id = parseInt(request.params.id)
     database.removeProductFromCart(id)
     response.sendStatus(200)
 })
 
-app.put('/api/cart/:id/:quantity', (request, response) => {
+app.put('/express-cart-api/cart/:id/:quantity', (request, response) => {
     const id = parseInt(request.params.id)
     const quantity = parseInt(request.params.quantity)
     const cartItem = database.setQuantity(id, quantity)
@@ -158,8 +158,8 @@ app.put('/api/cart/:id/:quantity', (request, response) => {
 })
 
 const portArg = parseInt(argv[2])
-const portDefault = 3001
+const portDefault = 27634
 const port = portArg || portDefault
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
-const baseUrl = `http://localhost:${port}`
+axios.defaults.baseURL = `http://localhost:${port}`
