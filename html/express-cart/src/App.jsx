6588a1d15cbb73053289709f56df8fa85d521d5d
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-11-04 23:33:39 -0600

    Remove DEBUG and make final build for express-cart

--- html/express-cart/src/App.jsx
+++ html/express-cart/src/App.jsx
@@ -8,8 +8,7 @@ import { useAsyncEffect } from './shared'
 import './App.css'
 
 export default function App() {
-    // DEBUG
-    const [products, forceUpdateProducts_DEBUG] = useFetchableProducts()
+    const products = useFetchableProducts()
     const [cartItems, addProductToCart, changeQuantity, removeCartItem] = useFetchableCartItems()
     
     const productElems = products.map((product) => {
@@ -36,24 +35,8 @@ export default function App() {
         />
     })
 
-    // DEBUG
-    const [axiosBaseUrl, _setAxiosBaseUrl] = useState("http://thedelta.stream:27633")
-    const setAxiosBaseUrl = (event) => {
-        const newBaseUrl = event.target.value
-        _setAxiosBaseUrl(newBaseUrl)
-        axios.defaults.baseURL = newBaseUrl
-        console.log("Axios base url updated:", newBaseUrl)
-    }
-
     return (
         <div className="App">
-            {/* // DEBUG */}
-            <input
-                value={axiosBaseUrl}
-                onChange={setAxiosBaseUrl}
-            />
-            <button onClick={forceUpdateProducts_DEBUG}>Force update products</button>
-
             <h1>Products</h1>
             {productElems}
             <h1>Cart</h1>
@@ -65,22 +48,13 @@ export default function App() {
 function useFetchableProducts() {
     const [products, setProducts] = useState([])
 
-    // DEBUG
-    const [forceUpdateCounter_DEBUG, setForceUpdateCounter_DEBUG] = useState(0)
-    
     useAsyncEffect(async () => {
-        // DEBUG
-        console.log("Updating products...")
         const response = await axios.get(`/express-cart-api/products`)
         const products = response.data
         setProducts(products)
-    }, [forceUpdateCounter_DEBUG])
+    }, [])
 
-    // DEBUG
-    const forceUpdateProducts_DEBUG = () => {
-        setForceUpdateCounter_DEBUG((last) => last + 1)
-    }
-    return [products, forceUpdateProducts_DEBUG]
+    return products
 }
 
 function useFetchableCartItems() {
